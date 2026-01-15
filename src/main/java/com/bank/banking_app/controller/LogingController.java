c6c31dbdc1231888a0f57c70e8bf19fb04e9b6e2
thenumi2006 <thenumi53@gmail.com>
2026-01-15 21:59:48 +0530

    create login page and create login controllers to Redirect root URL to login

---
+++ src/main/java/com/bank/banking_app/controller/LogingController.java
@@ -0,0 +1,18 @@
+package com.bank.banking_app.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LogingController {
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/")
+    public String root() {
+        return "redirect:/login";
+    }
+}
